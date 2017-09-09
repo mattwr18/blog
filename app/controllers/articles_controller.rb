47a2c4c707f4650cdde2717b517e76ce178f8529
Matt Rider <mattwr18@gmail.com>
2017-09-09 09:16:17 -0300

    remove password

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "Matt", password: "belugasfofas", except: [:index, :show]
+  http_basic_authenticate_with name: "Matt", password: "#{%&*@#{$}}", except: [:index, :show]
 
   def index
     @articles = Article.all
